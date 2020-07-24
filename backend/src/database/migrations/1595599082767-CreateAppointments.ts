b190d9a9648f7a46a3a49a93b305f4b0ebe0ec67
Danilo Ferreira <danilo1705@gmail.com>
2020-07-24 16:14:20 -0300

    TypeORM repository added

--- backend/src/database/migrations/1595599082767-CreateAppointments.ts
+++ backend/src/database/migrations/1595599082767-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1595599082767
                         type: 'varchar',
                         isPrimary: true,
                         generationStrategy: 'uuid',
+                        default: 'uuid_generate_v4()',
                     },
                     {
                         name: 'provider',
