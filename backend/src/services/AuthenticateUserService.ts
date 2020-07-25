2eeed741870076f804a9bae6b712345c100f8fb5
Danilo Ferreira <danilo1705@gmail.com>
2020-07-25 11:29:12 -0300

    Generating JWT token

--- backend/src/services/AuthenticateUserService.ts
+++ backend/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import User from '../models/User';
 
 interface Request {
@@ -9,6 +10,7 @@ interface Request {
 
 interface Response {
     user: User;
+    token: string;
 }
 
 class AuthenticateUserService {
@@ -27,8 +29,14 @@ class AuthenticateUserService {
             throw new Error('Incorrect email/paswword combination.');
         }
 
+        const token = sign({}, '2fa9e1fc8e7441bcc79efa9a0535ed38', {
+            subject: user.id,
+            expiresIn: '1d',
+        });
+
         return {
             user,
+            token,
         };
     }
 }
