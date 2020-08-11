34cee3c9904e687543cec1b7f6b3aad56dcfed9f
Danilo Ferreira <danilo1705@gmail.com>
2020-08-11 12:43:54 -0300

    Input and Button

--- frontend-mobile/src/pages/SignIn/index.tsx
+++ frontend-mobile/src/pages/SignIn/index.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import { Image } from 'react-native';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import { Container, Title } from './styles';
 
 import logoImg from '../../assets/logo.png';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Image source={logoImg} />
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
 
-    <Title>Faça seu logon</Title>
-  </Container>
-);
+      <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('deu');
+        }}
+      >
+        Entrar
+      </Button>
+    </Container>
+  );
+};
 
 export default SignIn;
