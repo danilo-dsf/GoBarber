070a5f64bc06d2d0a8a053a6463a6d50f1381b4a
Danilo Ferreira <danilo1705@gmail.com>
2020-09-01 07:46:22 -0300

    AppointmentsRepository updated

--- backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -62,6 +62,7 @@ class AppointmentsRopository implements IAppointmentsRepository {
             `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
+      relations: ['user'],
     });
 
     return appointments;
