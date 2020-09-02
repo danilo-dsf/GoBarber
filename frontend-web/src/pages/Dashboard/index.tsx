de543592e4a7066498a0e534408855a9929f7fad
Danilo Ferreira <danilo1705@gmail.com>
2020-09-02 07:59:20 -0300

    NextAppointment created on Dashboard page

--- frontend-web/src/pages/Dashboard/index.tsx
+++ frontend-web/src/pages/Dashboard/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+} from './styles';
 
 import logoImage from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
@@ -28,6 +37,34 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários Agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars2.githubusercontent.com/u/61921000?s=460&u=711d234772d64afd14a78c057f1b332d0b38afdb&v=4"
+                alt="Danilo Ferreira"
+              />
+
+              <strong>Danilo Ferreira</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
